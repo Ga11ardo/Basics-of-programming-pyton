96cfe7374f6e82f3f06506613f71eb899bc07b29
Ga11ardo <90717318+Ga11ardo@users.noreply.github.com>
2021-12-26 23:53:56 +0200

    Add files via upload

---
+++ BOPy9.py
@@ -0,0 +1,24 @@
+def type_sequence(str):
+    arr = []
+    for i in range(0,len(str)):
+        if str[i].isdigit():
+            arr.append(int(str[i]))
+    return arr
+
+def print_digits(arr):
+    print("Найдено "+str(len(arr))+" цифр(-а):", end =' ')
+    for i in arr:
+        print(str(i), end =' ')
+    print('')
+
+def receive_sum(arr):
+    sum = 0
+    for i in arr:
+        sum += i
+    return sum
+
+sequence = input("Введите последовательность: ")
+num = type_sequence(sequence) 
+print_digits(num)
+sum = receive_sum(num)
+print("Сумма цифр: " + str(sum))
